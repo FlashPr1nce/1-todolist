93c7b4483c559ebafe43fcdee68c29805490303f
FlashPr1nce <zalyatdinov99@gmail.com>
2024-03-28 14:49:35 +0200

    Here I have created two arrays that will send on props todo and I use their on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import Todolist from "./components/Todolist";
+import Todolist, {TasksType} from "./components/Todolist";
+
+
+let task1: TasksType[] = [
+    {id: 1, title: 'HTML', isDone: true},
+    {id: 2, title: 'CSS', isDone: true},
+    {id: 3, title: 'JS', isDone: false}
+]
+
+let task2: Array<TasksType> = [
+    {id: 4, title: 'Bread', isDone: false},
+    {id: 5, title: 'Beer', isDone: false},
+    {id: 6, title: 'Meat', isDone: true}
+]
 
 function App() {
     return (
         <div className="App">
-            <Todolist  title='What to buy'/>
-            <Todolist  title='What to learn'/>
-            <Todolist  title='Another'/>
+            <Todolist  title='What to learn' tasks={task1}/>
+            <Todolist  title='What to buy' tasks={task2}/>
         </div>
     );
 }
