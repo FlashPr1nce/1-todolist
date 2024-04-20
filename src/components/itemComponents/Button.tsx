598033fec87d6d5420da437fd19c6bd9566fe869
FlashPr1nce <zalyatdinov99@gmail.com>
2024-04-20 12:33:53 +0300

    added new props for buttons

--- src/components/itemComponents/Button.tsx
+++ src/components/itemComponents/Button.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { FilterValuesType } from '../../App';
 
 type ButtonPropsType = {
     title: string
-
     onClickHandler?: () => void
+    isDisabled?: boolean
+    classes?: string
 }
 
-export const Button = ({title, onClickHandler}:ButtonPropsType) => {
+export const Button = ({title, onClickHandler, isDisabled, classes}:ButtonPropsType) => {
     return (
-        <button onClick={onClickHandler}>
+        <button onClick={onClickHandler} disabled={isDisabled} className={classes}>
             {title}
         </button>
     );
